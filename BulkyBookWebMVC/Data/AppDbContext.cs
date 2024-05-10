248332a77590337af2f4e61bee11219625f68d01
Mr-Array <Mohamed.Nasser22@svu.edu.eg>
2024-05-10 05:46:22 +0300

    Add project files.

---
+++ BulkyBookWebMVC/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkyBookWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWebMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { }
+        public DbSet<Category> Categorys { get; set; }
+    }
+    
+}
---
+++ BulkyBookWebMVC/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWebMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DataTimeCreated { get; set; }   = DateTime.Now;
+
+    }
+}
